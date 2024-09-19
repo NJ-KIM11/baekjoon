b4ac0268ff7b727414041c69ef6d484fd516c9a5
NJ-KIM11 <alwaysred1116@gmail.com>
2024-09-19 19:05:25 +0900

    0919pm

---
+++ question/src/main/java/q1193/Main.java
@@ -0,0 +1,26 @@
+package q1193;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+
+//        int X = scr.nextInt();
+        int up=1;
+
+        for (int odd = 1; odd < 20; odd +=2) {
+            while (up <=odd){
+                System.out.print(up + " ");
+                up++;
+            }
+
+            while(up >1){
+                System.out.print(up + " ");
+                up--;
+            }
+
+        }
+
+    }
+}
